21313cd5a40c89af257b24bd6d02ff584b12e35e
MonoBear123 <sakovic001@gmail.com~>
2026-04-08 20:06:17 +0500

    Add

--- dags/tasks/movies/train_model.py
+++ dags/tasks/movies/train_model.py
@@ -81,14 +81,18 @@ def train():
         mlflow.log_metrics(metrics)
         signature = infer_signature(X_train, best.predict(X_train))
         mlflow.sklearn.log_model(best, name="sgd_model", signature=signature)
-        
+
         output = DATA_BASE / "output/movie"
         output.mkdir(parents=True, exist_ok=True)
         joblib.dump(best, DATA_BASE / "sgd_movies.skops")
         joblib.dump(scaler, DATA_BASE / "scaler_movies.skops")
         joblib.dump(power_trans, DATA_BASE / "power_trans_movies.skops")
 
+        output = DATA_BASE / "output/movie"
+        output.mkdir(exist_ok=True)
+        joblib.dump(best, DATA_BASE / "sgd_movies.skops")
+        joblib.dump(scaler, DATA_BASE / "scaler_movies.skops")
+        joblib.dump(power_trans, DATA_BASE / "power_trans_movies.skops")
+
 
 train()
-
-
